78af92c21d2752e63837016faa9d28e811f749d0
Diogo Lima <diogo@dr6.com.br>
2020-08-28 14:21:22 -0300

    add interval

--- index.js
+++ index.js
@@ -7,19 +7,18 @@ import pg from 'pg'
 
 const args = process.argv.slice(2)
 dotenv.config()
-const client = new pg.Client()
-await client.connect()
 const options = new chrome.Options().headless()
 const driver = await new selenium.Builder().forBrowser('chrome').setChromeOptions(options).build()
 
-function preConsultaPrecos(api_url, data, regiao) {
+const client = new pg.Client()
+await client.connect()
+
+async function preConsultaPrecos(api_url, data, regiao) {
     const d = data.split("/")
     const dia = `${d[2]}-${d[1]}-${d[0]}`
     const selectQuery = 'SELECT * FROM pldhorario where dia = $1 and submercado = $2'
 
-    client.query(
-        selectQuery,
-        [dia, regiao],
+    await makeQuery(selectQuery, [dia, regiao],
         async (err, res) => {
             if (err) {
                 console.log(err.stack)
@@ -27,8 +26,18 @@ function preConsultaPrecos(api_url, data, regiao) {
             }
             if (res.rowCount == 0) {
                 consultaPrecos(api_url, data, regiao)
+            } else {
+                console.log(dia, regiao, "-> Já existe no banco")
             }
         }
+    )
+}
+
+async function makeQuery(query, dados, callback) {
+    client.query(
+        query,
+        dados,
+        callback
     )
 }
 
@@ -36,22 +45,15 @@ async function adicionaEntrada(dados) {
     const insertQuery = 'INSERT INTO pldhorario (dia, hora, submercado, preco) VALUES($1, $2, $3, $4) RETURNING *'
     const updateQuery = 'UPDATE pldhorario SET dia = $1, hora = $2, submercado = $3, preco = $4 where dia = $1 and hora = $2 and submercado = $3 and preco = $4'
 
-    const client = new pg.Client()
-    await client.connect()
-
-    client.query(
-        updateQuery,
-        dados,
-        (err, res) => {
+    makeQuery(updateQuery, dados,
+        async (err, res) => {
             if (err) {
                 console.log(err.stack)
             }
             if (res.rowCount > 0) {
                 console.log(dados, "-> Atualizando")
             } else {
-                client.query(
-                    insertQuery,
-                    dados,
+                makeQuery(insertQuery, dados,
                     (err, _) => {
                         if (err) {
                             console.log(err.stack)
@@ -64,7 +66,23 @@ async function adicionaEntrada(dados) {
     )
 }
 
-async function processaPrecos(precos, data, regiao) {
+async function processaPrecos(data, regiao, api_url) {
+    const url = `${api_url}?periodo=${data}&aba=${regiao}`
+    let precos = []
+
+    await driver.get(url);
+    const tds = await driver.findElements(selenium.By.tagName('tr'));
+
+    for (let i=1; i<tds.length; i++) {
+        await tds[i].getText().then(s => {
+            let linha = s.split(" ")
+            precos[i-1] = []
+            linha.forEach(l => {
+                if (l !== '') precos[i-1].push(l)
+            })
+        });
+    }
+
     for (let i=0; i<precos.length; i++) {
         const p = precos[i]
         const d = data.split("/")
@@ -78,27 +96,16 @@ async function processaPrecos(precos, data, regiao) {
 }
 
 async function consultaPrecos(api_url, data, regiao) {
-    const url = `${api_url}?periodo=${data}&aba=${regiao}`
-    let precos = []
     try {
-        await driver.get(url);
-        const tds = await driver.findElements(selenium.By.tagName('tr'));
-        for (let i=1; i<tds.length; i++) {
-            await tds[i].getText().then(s => {
-                let linha = s.split(" ")
-                precos[i-1] = []
-                linha.forEach(l => {
-                    if (l !== '') precos[i-1].push(l)
-                })
-            });
-        }
-        processaPrecos(precos, data, regiao)
+        processaPrecos(data, regiao, api_url)
     } catch (e) {
         console.error(e);
     }
 }
 
-async function startCrawler() {
+async function startCrawler(args) {
+    console.log("Iniciando...")
+
     const api_url = "https://www.ccee.org.br/portal/faces/oracle/webcenter/portalapp/pages/publico/oquefazemos/produtos/precos/preco_horario_sombra_grafico.jspx"
 
     let data = [args[0]]
@@ -126,5 +133,5 @@ async function startCrawler() {
 
 }
 
-startCrawler()
-setInterval(startCrawler, 60 * 60 * 1000);
+startCrawler(args)
+setInterval(startCrawler, 60 * 60 * 1000)
